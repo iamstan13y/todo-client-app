397f0f3d338f10e5b1f6a7c5478b17e22ac073dc
Keith Stanley <iamstan13y@gmail.com>
2023-01-28 22:10:11 +0200

    clean up

--- Platforms/Windows/App.xaml.cs
+++ Platforms/Windows/App.xaml.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.UI.Xaml;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace Todo.Client.WinUI;
 
 /// <summary>

fe511fe8523b3a79829b723f59d4fc8880ab16b3
Keith Stanley <iamstan13y@gmail.com>
2023-01-29 23:09:55 +0200

    WIP.

---
+++ Models/ToDo.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace Todo.Client.Models
+{
+    public class ToDo : INotifyPropertyChanged
+    {
+        int _id;
+        string _name;
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}

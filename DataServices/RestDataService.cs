14e52afe465734c10c10da5a353c3a1f1d6673a4
Keith Stanley <iamstan13y@gmail.com>
2023-01-30 11:28:14 +0200

    Rest Data Service & Interface.

---
+++ DataServices/RestDataService.cs
@@ -0,0 +1,27 @@
+﻿using Todo.Client.Models;
+
+namespace Todo.Client.DataServices
+{
+    public class RestDataService : IRestDataService
+    {
+        public Task AddToDoAsync(ToDo toDo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteToDoAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ToDo>> GetAllToDosAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateToDoAsync(ToDo toDo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

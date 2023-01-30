324f845255d2f5015af7513cfe550a73e47bda57
Keith Stanley <iamstan13y@gmail.com>
2023-01-30 12:14:52 +0200

    Properties and stuff.

--- DataServices/RestDataService.cs
+++ DataServices/RestDataService.cs
@@ -1,9 +1,27 @@
-﻿using Todo.Client.Models;
+﻿using System.Text.Json;
+using Todo.Client.Models;
 
 namespace Todo.Client.DataServices
 {
     public class RestDataService : IRestDataService
     {
+        private readonly HttpClient _httpClient;
+        private readonly string _baseAddress;
+        private readonly string _url;
+        private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+        public RestDataService()
+        {
+            _httpClient = new HttpClient();
+            _baseAddress = DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5044" : "https://localhost:7044";
+            _url = $"{_baseAddress}/api";
+
+            _jsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+        }
+
         public Task AddToDoAsync(ToDo toDo)
         {
             throw new NotImplementedException();

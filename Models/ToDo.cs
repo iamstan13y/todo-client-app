ea80e774c381eae60ad6554e92731d70e25680af
Keith Stanley <iamstan13y@gmail.com>
2023-01-30 11:16:51 +0200

    Added Todo Model.

--- Models/ToDo.cs
+++ Models/ToDo.cs
@@ -5,7 +5,33 @@ namespace Todo.Client.Models
     public class ToDo : INotifyPropertyChanged
     {
         int _id;
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                if (_id == value)
+                    return;
+
+                _id = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Id)));
+            }
+        }
+
         string _name;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name == value)
+                    return;
+
+                _name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+            }
+        }
         public event PropertyChangedEventHandler PropertyChanged;
     }
 }

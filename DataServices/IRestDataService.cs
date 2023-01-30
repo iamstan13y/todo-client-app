ccf3a0f2b2bb713d95bd8772d851c1203fb9a04f
Keith Stanley <iamstan13y@gmail.com>
2023-01-30 11:28:10 +0200

    Rest Data Service & Interface.

---
+++ DataServices/IRestDataService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Todo.Client.Models;
+
+namespace Todo.Client.DataServices
+{
+    public interface IRestDataService
+    {
+        Task<List<ToDo>> GetAllToDosAsync();
+        Task AddToDoAsync(ToDo toDo);
+        Task UpdateToDoAsync(ToDo toDo);
+        Task DeleteToDoAsync(int id);
+    }
+}

f9f4d17812866c9cd3f8c00fc9a5dcb117226d3c
Keith Stanley <iamstan13y@gmail.com>
2023-01-31 12:01:41 +0200

    Manage Todo Page setup.

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,9 +1,13 @@
-﻿namespace Todo.Client;
+﻿using Todo.Client.Pages;
+
+namespace Todo.Client;
 
 public partial class AppShell : Shell
 {
     public AppShell()
     {
         InitializeComponent();
+
+        Routing.RegisterRoute(nameof(ManageToDoPage), typeof(ManageToDoPage));
     }
 }

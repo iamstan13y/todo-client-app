e2393d0b1ddd609704d91f09d7f3eaaca6597b58
Keith Stanley <iamstan13y@gmail.com>
2023-01-31 15:12:27 +0200

    Stuff!.

--- Pages/ManageToDoPage.xaml.cs
+++ Pages/ManageToDoPage.xaml.cs
@@ -1,9 +1,63 @@
+using System.Diagnostics;
+using Todo.Client.DataServices;
+using Todo.Client.Models;
+
 namespace Todo.Client.Pages;
 
+[QueryProperty(nameof(ToDo), "ToDo")]
 public partial class ManageToDoPage : ContentPage
 {
-	public ManageToDoPage()
-	{
-		InitializeComponent();
-	}
+    private readonly IRestDataService _dataService;
+    ToDo _toDo;
+    bool _isNew;
+
+    public ToDo ToDo
+    {
+        get => _toDo;
+        set
+        {
+            _isNew = IsNew(value);
+            _toDo = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public ManageToDoPage(IRestDataService dataService)
+    {
+        InitializeComponent();
+
+        _dataService = dataService;
+        BindingContext = this;
+    }
+
+    bool IsNew(ToDo toDo)
+    {
+        if (toDo.Id == 0)
+            return true;
+        return false;
+    }
+
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        if (_isNew)
+        {
+            Debug.WriteLine("---> Add New Item.");
+            await _dataService.AddToDoAsync(ToDo);
+        } 
+        else
+        {
+            Debug.WriteLine("---> Add New Item.");
+            await _dataService.AddToDoAsync(ToDo);
+        }
+    }
+
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        await _dataService.DeleteToDoAsync(ToDo.Id);
+        await Shell.Current.GoToAsync("..");
+    }
+    async void OnCancelButtonClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("..");
+    }
 }
